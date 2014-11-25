e65c3b3b93055946ef7bd576fcb961db2bfac25f
Simon <simon.berda@gmail.com>
2014-11-25 19:28:43 +0100

    Supression  Struct et quelque opera
    Supr Struct
    Supr quelques operateurs surchargés

--- include/node_iterator.hpp
+++ include/node_iterator.hpp
@@ -1,19 +1,11 @@
 #pragma once 
 #include <cstddef> // ptrdiff_t
 template<typename T>
-class node_iterator;
+class node_iterator
 {
     public:
 	
-		struct Node_Structure //Comme tout ses membre doivent etre public, on utilisera la mot cle struct pour la declarer
-		{
-			typedef ptrdiff_t difference_type;
-			//iterator_category?
-			typedef T value_type;
-			typedef T* pointer;// I Dunno
-			typedef T& reference;// I Dunno
-			typedef T Data; //The Data of the node
-		};
+
 		
 		node_iterator::node_iterator() //Constructeur multiparametre
 		{
@@ -64,74 +56,7 @@ class node_iterator;
 		{
 		// WIP
 		}
-		
-		/*
-			http://en.wikibooks.org/wiki/C%2B%2B_Programming/Operators/Operator_Overloading
-			Increment and decrement have two forms, prefix (++i) and postfix (i++). 
-			To differentiate, the postfix version takes a dummy integer. Increment and decrement operators are most often member functions, as they generally need access to the private member data in the class. 
-			The prefix version in general should return a reference to the changed object. 
-			The postfix version should just return a copy of the original value. In a perfect world, A += 1, A = A + 1, A++, ++A should all leave A with the same value.	
-		*/
-		
-		SomeValue& SomeValue::operator++() // prefix
-		{
-			
-			//la preincrementation passe au noeud suivant et renvoie une reference
-			
-			
-		
-			++data;
-			return *this;
-		}
- 
-		SomeValue SomeValue::operator++(int unused) // postfix
-		{
-			//la post incrementation renvoie une copie de l'iterateur, puis passe l'iterateur au noeud suivant.
 
-			SomeValue result = *this;
-			++data;
-			return result;
-		}
-		
-		
-		SomeValue& SomeValue::operator--() // prefix
-		{
-			//la preincrementation passe au noeud suivant et renvoie une reference
-			--data;
-			return *this;
-		}
- 
-		SomeValue SomeValue::operator--(int unused) // postfix
-		{
-			//la post incrementation renvoie une copie de l'iterateur, puis passe l'iterateur au noeud suivant.
-			SomeValue result = *this;
-			--data;
-			return result;
-		}
-		
-		
-		
-		
-		
-
-		bool are_equal (node_iterator node1, node_iterator node2)
-		{
-			//WIP
-		}
-		
-
-		bool is_lower (node_iterator node1, node_iterator node2)
-		{
-			//WIP
-		}
-
-		bool is_bigger (node_iterator node1, node_iterator node2)
-		{
-			//WIP
-		}   
-
-    protected:
-    private:
 };
 
 
