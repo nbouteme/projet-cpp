5ab45c622e9998d3117f7e6c1d9cb87a3810656b
ibenroui <imade83@gmail.com>
2014-11-26 13:21:11 +0100

    Implémentation de la struct node

--- include/List.hpp
+++ include/List.hpp
@@ -5,8 +5,23 @@ namespace nsSdD
     template <typename T>
     class List
     {
+        struct node
+        {
+    	    typedef node* pointer;
     
-    public:
-    protected:
+        	pointer prev = nullptr;
+        	pointer next = nullptr;
+        	T data;
+        
+        	void hook (pointer elem)
+        	{
+        		pointer tmp = elem->prev;
+        		next = elem;
+        		prev = elem->prev;
+        		elem->prev = this;
+        		if(tmp)
+        		tmp->next = this;
+        	}
+        };
     };
 }
