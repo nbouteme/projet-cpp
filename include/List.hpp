d9bfe9c81189f8463ba1792460fa45c8b42b2283
SBerda <b13006192@squeeze.iut.univ-aix.fr>
2014-11-28 15:27:44 +0100

    Implemente node_iterator + typedef

--- include/List.hpp
+++ include/List.hpp
@@ -23,5 +23,59 @@ namespace nsSdD
         		tmp->next = this;
         	}
         };
+
+        struct node_iterator
+        {
+			typedef T         value_type;
+			typedef ptrdiff_t difference_type;
+			typedef base_node* pointer;
+			typedef node_iterator& reference;
+			typedef std::bidirectional_iterator_tag	iterator_category;
+			
+			node_iterator(const base_node* val = nullptr) : node_ptr(val) {}	
+			pointer node_ptr;
+			
+			node_iterator& operator++() ()
+			{
+				node_ptr = node_ptr->next;
+				return *this;
+			}
+			
+			node_iterator operator++() (int)
+			{
+				base_node* tmp = node_ptr;
+				node_ptr = node_ptr->next;
+				return tmp;
+			}
+			
+			node_iterator operator--() (int)
+			{
+				base_node* tmp = node_ptr;
+				node_ptr = node_ptr->prev;
+				return tmp;
+			}
+			
+			bool operator ==() (node_iterator node)
+			{
+				return node_ptr == node.node_ptr;
+			}	
+			
+			bool operator!=() (node_iterator node)
+			{
+				return node_ptr != node.node_ptr;
+			}	
+			
+			node_iterator& operator--() ()
+			{
+				node_ptr = node_ptr->prev;
+				return *this;
+			}
+			
+			value_type& operator*() ()
+			{
+				return reinterpret_cast<node*>(node_ptr)->data;
+			}
+			
+		};	
     };
 }
