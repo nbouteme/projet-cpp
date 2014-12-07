0d082e82078f952f37eb4f5ee6f0bf400abdbc95
fuyukaidesu <hue@pomf.fr>
2014-12-07 01:14:52 +0100

    HTML Doxygen

---
+++ build/src/CMakeFiles/projet-cpp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/projet-cpp.dir/main.cpp.o"
+  "../projet-cpp.pdb"
+  "../projet-cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/projet-cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
